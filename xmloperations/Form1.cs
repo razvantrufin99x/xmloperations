ed7a99b529e9bed00e276283a6f7196b396e9d19
RAZVAN TRUFIN <razvantrufin99x@gmail.com>
2022-12-02 00:26:07 +0200

    Add project files.

---
+++ xmloperations/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace xmloperations
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(Application.StartupPath + "/Employees.xml");
+            TreeNode root = new TreeNode(doc.DocumentElement.Name);
+            treeView1.Nodes.Add(root);
+            foreach (XmlNode node in doc.DocumentElement.ChildNodes)
+            {
+                TreeNode employee = new TreeNode("Employee ID :" +
+                node.Attributes["employeeid"].Value);
+                root.Nodes.Add(employee);
+                if (node.HasChildNodes)
+                {
+                    foreach (XmlNode childnode in node.ChildNodes)
+                    {
+                        TreeNode n2 = new TreeNode(childnode.Name + " : " + childnode.InnerText);
+                        employee.Nodes.Add(n2);
+                    }
+                }
+            }
+        }
+    }
+}
